11eb1b3d0d2558380fd628863fdf0f28c52f7755
Abhisek Das <dasabhisek668@gmail.com>
2025-07-26 00:28:07 +0530

    Created Home , movies page

---
+++ client/src/pages/SeatLayout.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
+
+import Loading from '../components/Loading';
+
+const SeatLayout = () => {
+
+  const {id , date} = useParams();
+
+  const [selectedSeats , setSelectedSeats] = useState([]);
+  const [selectedTime , setSelectedTime] = useState(null);
+  const [show , setShow] = useState(null);
+
+  const navigate = useNavigate();
+
+  const getShow = async () => {
+
+    const show = dummyShowsData.find((show) => show._id === id);
+
+    if(show){
+
+      setShow({
+        movie: show,
+        dateTime : dummyDateTimeData
+      })
+    }
+    
+  }
+
+  useEffect(() => {
+    getShow();
+  } , [])
+
+
+  return show ? (
+    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
+
+      {/**Available timing */}
+      <div className="w-60 ">
+
+      </div>
+
+      {/**Seat Layout */}
+      <div className="">
+
+      </div>
+
+      
+    </div>
+  ) : (
+    <Loading />
+  )
+}
+
+export default SeatLayout
