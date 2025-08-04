6e310468bb4d56b0051609633faaa0dfb2621cdb
AbhisekDas01 <dasabhisek668@gmail.com>
2025-08-04 23:51:19 +0530

    ingest updated

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -57,5 +57,6 @@ const syncUserUpdation = inngest.createFunction(
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
-    syncUserDeletion
+    syncUserDeletion,
+    syncUserUpdation
 ];
